3126bb697d2504973a8ac8a73fd1036a9b034451
FaisalBaskaraNugraha <111202214468@mhs.dinus.ac.id>
2025-06-02 01:30:27 +0700

    DONE: Migration & Model

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,40 +9,57 @@ use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+/** @use HasFactory<\Database\Factories\UserFactory> */
+use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
-    protected $fillable = [
+/**
+    * The attributes that are mass assignable.
+    *
+    * @var list<string>
+    */
+protected $fillable = [
         'name',
-        'email',
-        'password',
-    ];
+        'nama',
+'email',
+'password',
+        'role',
+        'alamat',
+        'no_ktp',
+        'no_hp',
+        'no_rm',
+        'poli',
+];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+/**
+    * The attributes that should be hidden for serialization.
+    *
+    * @var list<string>
+    */
+protected $hidden = [
+'password',
+'remember_token',
+];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
+/**
+    * Get the attributes that should be cast.
+    *
+    * @return array<string, string>
+    */
+protected function casts(): array
+{
+return [
+'email_verified_at' => 'datetime',
+'password' => 'hashed',
+];
+}
+
+    public function jadwalPeriksas()
     {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
+        return $this->hasMany(JadwalPeriksa::class, 'id_dokter');
+    }
+
+    public function janjiPeriksas()
+    {
+        return $this->hasMany(JanjiPeriksa::class, 'id_pasien');
     }
 }
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -6,44 +6,52 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
+/**
+    * Run the migrations.
+    */
+public function up(): void
+{
+Schema::create('users', function (Blueprint $table) {
+$table->id();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('nama', 255);
+$table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
-        });
+$table->string('password');
+            $table->enum('role', ['pasien', 'dokter']);
+            $table->string('alamat', 255);
+            $table->string('no_ktp', 255);
+            $table->string('no_hp', 50);
+            $table->string('no_rm', 25)->nullable();
+            $table->string('poli', 50)->nullable();
+            $table->timestamp('email_verified_at')->nullable();
+$table->rememberToken();
+$table->timestamps();
+});
 
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
-        });
+Schema::create('password_reset_tokens', function (Blueprint $table) {
+$table->string('email')->primary();
+$table->string('token');
+$table->timestamp('created_at')->nullable();
+});
 
-        Schema::create('sessions', function (Blueprint $table) {
-            $table->string('id')->primary();
-            $table->foreignId('user_id')->nullable()->index();
-            $table->string('ip_address', 45)->nullable();
-            $table->text('user_agent')->nullable();
-            $table->longText('payload');
-            $table->integer('last_activity')->index();
-        });
-    }
+Schema::create('sessions', function (Blueprint $table) {
+$table->string('id')->primary();
+$table->foreignId('user_id')->nullable()->index();
+$table->string('ip_address', 45)->nullable();
+$table->text('user_agent')->nullable();
+$table->longText('payload');
+$table->integer('last_activity')->index();
+});
+}
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('users');
-        Schema::dropIfExists('password_reset_tokens');
-        Schema::dropIfExists('sessions');
-    }
+/**
+    * Reverse the migrations.
+    */
+public function down(): void
+{
+Schema::dropIfExists('users');
+Schema::dropIfExists('password_reset_tokens');
+Schema::dropIfExists('sessions');
+}
 };
